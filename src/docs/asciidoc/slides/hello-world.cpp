f364234d0c08c96dd711cabbd2a3818e403b199d
Jez Higgins <jez@jezuk.co.uk>
2020-02-09 16:29:36 +0000

    some stuff on values and references, start of raii, split up files

---
+++ src/docs/asciidoc/slides/hello-world.cpp
@@ -0,0 +1,33 @@
+== Hello, World!
+
+[source,cpp]
+--
+
+int main() {
+}
+
+--
+
+[NOTE.speaker]
+--
+Here is the minimal C++ program.
+--
+
+== Hello, World!
+
+[source,cpp]
+--
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+}
+
+--
+
+[NOTE.speaker]
+--
+Hello world!
+
+--
